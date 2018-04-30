5e6a238af075406f24ce337c2d9c95c3dd86927c
unknown <novikov20021201@gmail.com>
2018-04-30 14:17:50 +0300

    HTML model of user`s cabinet

---
+++ cabinetScript.js
@@ -0,0 +1,43 @@
+function tableMouseOver() {
+	document.getElementById("table").setAttribute("src", "CabinetImages/ДоскаНавед.png");
+}
+function computerMouseOver() {
+	document.getElementById("computer").setAttribute("src", "CabinetImages/компНАвед.png");
+}
+function laptopMouseOver() {
+	document.getElementById("laptop").setAttribute("src", "CabinetImages/НоутНавед.png");
+}
+function ZNOMouseOver() {
+	document.getElementById("ZNO").setAttribute("src", "CabinetImages/ЗНОНАВЕД.png");
+}
+function briefcaseMouseOver() {
+	document.getElementById("briefcase").setAttribute("src", "CabinetImages/навед.png");
+}
+
+
+function tableMouseOut() {
+	document.getElementById("table").setAttribute("src", "CabinetImages/Доска.jpg");
+}
+function computerMouseOut() {
+	document.getElementById("computer").setAttribute("src", "CabinetImages/комп.png");
+}
+function laptopMouseOut() {
+	document.getElementById("laptop").setAttribute("src", "CabinetImages/Ноут.png");
+}
+function ZNOMouseOut() {
+	document.getElementById("ZNO").setAttribute("src", "CabinetImages/ЗНО.png");
+}
+function briefcaseMouseOut() {
+	document.getElementById("briefcase").setAttribute("src", "CabinetImages/Порфтель.png");
+}
+
+function tableClick() {
+}
+function computerClick() {
+}
+function laptopClick() {
+}
+function ZNOClick() {
+}
+function briefcaseClick() {
+}
